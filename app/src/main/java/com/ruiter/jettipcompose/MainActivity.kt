e0b243d3bccb7632ecb0858cd87d40ba112f88f2
Ruiter Matos <ruitercomp@gmail.com>
2022-01-18 15:00:50 -0300

    add top header composable

--- app/src/main/java/com/ruiter/jettipcompose/MainActivity.kt
+++ app/src/main/java/com/ruiter/jettipcompose/MainActivity.kt
@@ -3,11 +3,19 @@ package com.ruiter.jettipcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ruiter.jettipcompose.ui.theme.JetTipComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +23,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello World")
+                TopHeader()
             }
         }
     }
@@ -27,6 +35,32 @@ fun MyApp(content: @Composable () -> Unit) {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
             content()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFBC89FF)
+    ) {
+        val total = "%.2f".format(totalPerPerson)
+
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(text = "Total per person",
+            style = MaterialTheme.typography.h5)
+            Text(text = "$$total",
+            style = MaterialTheme.typography.h4)
         }
     }
 }
