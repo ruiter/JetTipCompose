fda334d32b200bba1ca0eb3f0498ee10a3f3e954
Ruiter Matos <ruitercomp@gmail.com>
2022-01-18 21:14:26 -0300

    add input field to the main content

--- app/src/main/java/com/ruiter/jettipcompose/MainActivity.kt
+++ app/src/main/java/com/ruiter/jettipcompose/MainActivity.kt
@@ -8,24 +8,32 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ruiter.jettipcompose.components.InputField
 import com.ruiter.jettipcompose.ui.theme.JetTipComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+                //TopHeader()
+                MainContent()
             }
         }
     }
@@ -71,9 +79,20 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
     }
 }
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
 fun MainContent() {
+    val totalPaymentState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalPaymentState.value) {
+        totalPaymentState.value.trim().isNotEmpty()
+    }
+
+    val keyBoardController = LocalSoftwareKeyboardController.current
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -82,7 +101,16 @@ fun MainContent() {
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column() {
-
+            InputField(
+                valueState = totalPaymentState,
+                labelId = "Enter Payment",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    keyBoardController?.hide()
+                }
+            )
         }
     }
 }
